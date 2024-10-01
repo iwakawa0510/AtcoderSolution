d102bbcc2f847e4fa8c565def961ae813b782858
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2024-10-01 18:24:24 +0900

    20241001進捗

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AtcoderSolution
+test
---
+++ aizu02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+ int x;
+  cin >> x; 
+  cout << x*x*x << endl;
+  return 0;
+}
