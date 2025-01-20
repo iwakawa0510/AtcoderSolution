91dadf906892c8c07085dda661f64475132789f2
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-01-21 04:10:17 +0900

    abc381b wa

--- abc381/b/main.cpp
+++ abc381/b/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
+int No(){
+    cout << "No" << endl;
+    exit(0);
+}
+
+int Yes(){
+    cout << "Yes" << endl;
+    exit(0);
+}
+
 int main(){ 
+    string str;
+    cin >> str;
+    if(!(str.size()%2 == 0))No();
+
+    for(int i = 0;i <= (str.size()/2);i++){
+        if(str[i] == str[i+1] && !(str[i+1] == str[i+2])){
+            Yes();
+        }
+    }
     return 0;
 }
