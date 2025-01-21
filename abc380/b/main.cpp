f34d71308475bc3f33c6ba3fba4b518ae58d8e42
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-01-21 23:24:09 +0900

    abc380b wa

--- abc380/b/main.cpp
+++ abc380/b/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main(){ 
-    return 0;
+    string str;
+    cin >> str;
+    
+    int mem[110];
+    for(int i = 0;i < 110;i++){
+        int hyph_cnt = 0;
+        int val_cnt = 0;
+     for(int j = 0;j < str.size();j++){
+        if(str[j] == "-"){
+            hyph_cnt++;
+        }else if(str[j] == "|"){
+            val_cnt++;
+            if(val_cnt >= 2){
+                mem[i] = hyph_cnt;
+                continue;
+             }
+            }
+        }
+    }return 0;
 }
