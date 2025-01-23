b37f3a93b01537b47d5e9d981dc9c471efc9cf9f
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-01-23 22:19:58 +0900

    0123e abc379-b wa

--- abc378/b/main.cpp
+++ abc378/b/main.cpp
@@ -4,5 +4,24 @@
 using namespace std;
 
 int main(){ 
+    //入力,
+    int n,m;
+    cin >> n;
+    int q[110] = {};
+    int r[110] = {};
+    for(int i = 0;i <= n;i++){
+        if(i == 0)continue;
+        cin >> q[i] >> r[i];
+    }
+    cin >> m;    
+    while(m--){
+    int t,d;
+    cin >> t >> d;
+    t--;
+    int b = d / q[t], c = d % q[t];
+    int a = c <= r[t] ? b : b + 1;
+    int ans = a * q[t] + r[t];
+    cout << ans << endl;
+    }
     return 0;
 }
