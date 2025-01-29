c1116c6b9b9867c3a80ae2dfb9116195abbdfa3a
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-01-29 23:48:03 +0900

    0129 abc374-b wa

--- abc374/b/main.cpp
+++ abc374/b/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-int main(){ 
+int main(){
+    string S,T;
+    cin >> S >> T;
+    int ans = 0;
+    int n = min(S.size(),T.size());
+    for(int i = 0;i < n;i++){
+        if(!S[i] == T[i]){
+            i++;
+            ans += i;
+            cout << ans << endl;
+            return 0;
+        }
+    }
+    if(S.size() != T.size()){
+        cout << (n+1) << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
     return 0;
 }
