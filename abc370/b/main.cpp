1416e735a364ae8f9ea89f9c36d34b5fe028a2ba
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-02-05 01:28:01 +0900

    0204e abc370-b

--- abc370/b/main.cpp
+++ abc370/b/main.cpp
@@ -8,5 +8,21 @@
 using namespace std;
 
 int main(){ 
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n,vector<int>(n));
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < (i+1);j++){
+            cin >> a[i][j];
+            a[i][j]--;
+            a[j][i] = a[i][j];
+        }
+    }
+    int ans = 0;
+    for(int i = 0;i < n;i++){
+        ans = a[ans][i];
+    }
+    ans += 1;
+    cout << ans << endl;
     return 0;
 }
