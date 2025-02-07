f58a5081e25821cb5f94f4da3e004b01df11be8a
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-02-08 01:37:58 +0900

    abc367-b ac

---
+++ abc367/b/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){ 
+    float x;
+    cin >> x;
+    cout << x << endl;
+    return 0;
+}
---
+++ abc367/b/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){ 
+    float x;
+    cin >> x;
+    
+    cout << x << endl;
+    return 0;
+}
