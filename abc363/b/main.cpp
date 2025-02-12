4bf2d5639348e7bf387aa8325a76a955bb1e425e
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-02-12 22:54:31 +0900

    daily : abc363-b ac #0212e

---
+++ abc363/b/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){ 
+    int n,t,p;
+    cin >> n >> t >> p;
+    vector <int> L(n);
+    for(int i = 0;i < n;i++){
+        cin >> L[i];
+    }
+    int ans = 0; 
+    while(true){
+        int cnt_p = 0;
+        for(int i = 0;i < n;i++){
+            L[i]++;
+            if(L[i] >= t)cnt_p++;
+        }
+        if(cnt_p >= p){break;
+        }
+        ans++;
+    }
+    if(ans == 0){cout << ans << endl;
+    return 0;
+    }
+    else{cout << ans+1 << endl;
+    return 0;
+    }
+}
