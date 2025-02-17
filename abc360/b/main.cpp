498b93bef826ce4d6396f1e69f7a402e430252cf
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-02-17 22:36:31 +0900

    daily :abc360b wa 0217

---
+++ abc360/b/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){ 
+    string S,T;
+    cin >> S >> T;
+    
+    for(int w = 1;w < S.size();w++){
+        for(int c = 0;c < w;c++){
+            string now = "";
+            for(int i = c;i < s.size();i += w){
+                now += S[i];
+            } 
+            if(now == T){
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
