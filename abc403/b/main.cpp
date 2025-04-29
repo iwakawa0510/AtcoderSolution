0e7ed4351cb00e5be68883ed082ec1bffa40ac79
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-04-30 00:58:57 +0900

    anscopy:abc403b 0429

--- abc403/b/main.cpp
+++ abc403/b/main.cpp
@@ -11,6 +11,22 @@ using namespace std;
 int main(){ 
     string t,u;
     cin >> t >> u;
-    
+    int n,m;
+    n = t.size();
+    m = u.size();
+    for(int i = 0;i <= n - m;i++){
+        bool ok = true;
+        for(int j = 0;j < m;j++){
+            if(t[i+j] != '?' && t[i+j] != u[j]){
+                ok = false;
+                break;
+            }
+        }
+        if(ok){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
