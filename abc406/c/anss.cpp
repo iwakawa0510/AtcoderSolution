2b0ffc657a59fb64955e7eb87dd46b3cbecad3b0
iwkawa0510 <ibent.toppa.sitai@gmail.com>
2025-05-31 02:27:12 +0900

    review:abc406c 0530

---
+++ abc406/c/anss.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <utility> // pairのため
+
+using namespace std;
+using ll = long long;
+
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> p(n);
+  for (int i = 0; i < (n); ++i) cin >> p[i];
+
+  vector<int> d;
+  for (int i = 0; i < (n-1); ++i) d.push_back((p[i] < p[i+1]) ? 0 : 1);
+
+  using P = pair<int,int>;
+  vector<P> rle;
+  for (int x : d) {
+    if (rle.size() && rle.back().first == x) rle.back().second++;
+    else rle.emplace_back(x, 1);
+  }
+
+  ll ans = 0;
+  for (int i = 0; i < rle.size(); ++i) {
+    if (rle[i].first == 1) {
+      ll l = 0, r = 0;
+      if (0 < i) l = rle[i-1].second;
+      if (i+1 < rle.size()) r = rle[i+1].second;
+      ans += l*r;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
